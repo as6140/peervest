f5aee1dced7020d795d2561c78940a9ecd8acfd7
Alex Shropshire <as6140@nyu.edu>
2019-07-28 09:18:20 -0700

    add dollar sign to loan_amnt & funded_amnt outputs

--- app.py
+++ app.py
@@ -37,8 +37,8 @@ def output():
     rec_table_ranked['shrop_ratio'] = round(rec_table_ranked['shrop_ratio'],2)
     rec_table_ranked['prob_default'] = round(rec_table_ranked['prob_default'],3)
     rec_table_ranked['return_preds'] = round(rec_table_ranked['return_preds'],3)
-    rec_table_ranked['loan_amnt'] = '$' + str(rec_table_ranked['loan_amnt'])
-    rec_table_ranked['funded_amnt'] = '$' + str(round(rec_table_ranked['funded_amnt'],0))
+    rec_table_ranked['loan_amnt'] = '${:,.2f}'.format(rec_table_ranked['loan_amnt'])
+    rec_table_ranked['funded_amnt'] = '${:,.2f}'.format(round(rec_table_ranked['funded_amnt'],0))
 
     #choose which columns are returned in printed table
     tabl = rec_table_ranked[['shrop_ratio','prob_default','return_preds',
